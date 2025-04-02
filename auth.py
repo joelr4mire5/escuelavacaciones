62bc81aeb84626a3212d5b7a99abbf0a15d097dc
joelr4mire5 <joel.r4mire5@outlook.com>
2025-04-01 20:42:56 -0600

    adding teachers

--- auth.py
+++ auth.py
@@ -3,7 +3,26 @@
 # Usuarios hardcodeados (puedes mover esto a la BD si quieres)
 USERS = {
     "maestro1": "1234",
-    "maestra2": "abcd"
+    "maestro2": "1234",
+    "maestro3": "1234",
+    "maestro4": "1234",
+    "maestro5": "1234",
+    "maestro6": "1234",
+    "maestro7": "1234",
+    "maestro8": "1234",
+    "maestro9": "1234",
+    "maestro10": "1234",
+    "maestro11": "1234",
+    "maestro12": "1234",
+    "maestro13": "1234",
+    "maestro14": "1234",
+    "maestro15": "1234",
+    "maestro16": "1234",
+    "maestro17": "1234",
+    "maestro18": "1234",
+    "maestro19": "1234",
+    "maestro20": "1234"
+
 }
 
 def validate_login(username, password):
